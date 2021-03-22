99a5c9d0434b29e3853b5dc890478f56d16d64b9
Gabriel Bento <Gabriel_holanda@hotmail.com.br>
2021-03-21 21:49:35 -0400

    Fixing erros II

--- Scripts/Explosion.gd
+++ Scripts/Explosion.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Node2D
 
 var stage: Stage1
 
@@ -13,16 +13,21 @@ func _ready():
 
 func create_explosion(pos: Vector2):
 	# Configura a posição
-	pos -= Vector2(32, 2)
+	$"Center".position = pos
 	explosion_position(pos)
 	stage.add_child_below_node(stage.get_node("Wall"), self)
 
+	explosion_position(pos)
+	$Stub.queue_free()
+	$Tail.queue_free()
 
 # Configura o tamanho da explosão
 func explosion_position(pos_bomb: Vector2):
 	# Rotação
 	var rot = 0
-	$".".position = pos_bomb
+	
+	pos_bomb -= Vector2(6*16 + 10, -9)
+	
 	var positions = [Vector2(0, 16), Vector2(-16, 0), Vector2(0, -16), Vector2(16, 0)]
 	var node = []
 	for i in range(0, 4):
