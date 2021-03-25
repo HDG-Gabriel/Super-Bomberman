ae7ca0eeaf837ced3de99665a72fdc9759267dd0
Gabriel Bento <Gabriel_holanda@hotmail.com.br>
2021-03-24 23:13:58 -0400

    Add buffer speed

--- Scripts/Item.gd
+++ Scripts/Item.gd
@@ -8,9 +8,22 @@ func _ready():
 
 func choose_animation():
 	var anim = ["Life", "Power", "Speed", "Bomb"]
-	$AnimatedSprite.play(anim[randi()%4])
+	$AnimatedSprite.play(anim[2])
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Item_body_entered(body):
+	if body.is_in_group("Player"):
+		if $AnimatedSprite.animation == "Speed":
+			buffer_speed(body)
+
+
+# Aumenta a velocidade do jogador
+func buffer_speed(body: Node2D):
+	$CollisionShape2D.set_deferred("disabled", true)
+	body.velocity += 20
+	queue_free()
