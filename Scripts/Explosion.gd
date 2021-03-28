7c609fb89225a0ced214461582921d534bd5d57f
Gabriel Bento <Gabriel_holanda@hotmail.com.br>
2021-03-28 18:11:20 -0400

    Removed old animations in explosion

--- Scripts/Explosion.gd
+++ Scripts/Explosion.gd
@@ -22,32 +22,7 @@ func create_explosion(pos: Vector2):
 	stage.add_child_below_node(stage.get_node("Wall"), self)
 	$SoundExplosion.play()
 	$"Center".position = pos
-	explosion_position(pos)
 	$Center.get_node("AnimatedSprite").play("Center")
-
-	$Stub.queue_free()
-
-# Configura o tamanho da explosão
-func explosion_position(pos_bomb: Vector2):
-	# Rotação
-	var rot = 0
-	
-	var positions = [Vector2(0, 14), Vector2(-15, -1), Vector2(0, -15), Vector2(15, 0)]
-	var node = []
-	for i in range(0, 4):
-		node.append($Stub.duplicate())
-		node[i].position = pos_bomb + positions[i]
-		add_child(node[i])
-		
-		node[i].get_node("AnimatedSprite").play("Stub")
-
-		if i > 0:
-			rot += 90
-			node[i].get_node("AnimatedSprite").rotation_degrees = rot
-			
-			# Rotaciona o colisor tbm
-			if (rot / 90) % 2 == 1:
-				node[i].get_node("CollisionShape2D").rotation_degrees = 90
 
 
 func _on_Center_body_entered(body):
