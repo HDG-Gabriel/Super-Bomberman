120523bbf7002aecdb6d0f3e2fed60cd650ac618
Gabriel Bento <Gabriel_holanda@hotmail.com.br>
2021-03-28 20:44:39 -0400

    Body explosion created

--- Scripts/Explosion.gd
+++ Scripts/Explosion.gd
@@ -2,15 +2,16 @@ extends Node2D
 
 var stage: Stage1
 
+# Sentido: baixo, esquerda, cima, direita
+var sides = [Vector2(0, 16), Vector2(-16, 0), Vector2(0, -16), Vector2(16, 0)]
+
 # Determina o tamanho da explosão
 var damage: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Por padrão a explosão inicia oculta, para que quando
-	# parte coloidir com outras coisas, a chama seja destruída
-	hide()
-	$Timer.start()
+	# parte colidir com outras coisas, a chama seja destruída
 	damage = stage.player.damage
 
 
@@ -20,17 +21,32 @@ func _ready():
 
 func create_explosion(pos: Vector2):
 	stage.add_child_below_node(stage.get_node("Wall"), self)
-	$SoundExplosion.play()
+	#$SoundExplosion.play()
 	$"Center".position = pos
-	$Center.get_node("AnimatedSprite").play("Center")
+	create_body_explosion()
+	create_stubs()
+	destroy_nodes()
 
 
-func _on_Center_body_entered(body):
-	kill_player(body)
+func create_body_explosion():
+	for i in range(0, 4):
+		if i % 2 == 0:
+			for j in range(1, damage + 1):
+				create_node($Tail_Vertical, $Center.position + j*sides[i])
+		else:
+			for j in range(1, damage + 1):
+				create_node($Tail_Horizontal, $Center.position + j*sides[i])
 
 
-func _on_Stub_body_entered(body):
-	kill_player(body)
+func create_node(area: Area2D, pos: Vector2):
+	var node = area.duplicate()
+	node.position = pos
+	add_child(node)
+
+
+# Cria as "pontas" da explosão
+func create_stubs():
+	pass
 
 
 # Verifica se pode matar o player, se puder, mata,
@@ -40,12 +56,9 @@ func kill_player(body):
 		body.take_damage()
 
 
-# Quando a animação de explosão terminar ela será destruida
-func _on_AnimatedSprite_animation_finished():
-	queue_free()
+func destroy_nodes():
+	var nodes = [$Tail_Horizontal, $Tail_Vertical,
+	$Stub_Down, $Stub_Left, $Stub_Right, $Stub_Up]
 
-
-# Torna vísivel a explosão, se não colidir com nada
-func _on_Timer_timeout():
-	if visible == false:
-		show()
+	for node in nodes:
+		node.queue_free()
